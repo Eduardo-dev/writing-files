c49d8e40960f4d0f89b89a4ec11f84ed6d13775d
Eduardo Nunes <eduardoafonso0519@gmail.com>
2019-02-19 19:25:06 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite o caminho da Pasta: ");
+		String pathStr= input.nextLine();
+		System.out.print("Digite o nome da pasta: ");
+		String folderNameStr = input.nextLine();
+		System.out.println("Digite o nome do arquivo de texto: ");
+		String fileNameStr = input.nextLine();
+		
+		File targetFolder = new File(pathStr +"\\" + folderNameStr);
+		targetFolder.mkdir();
+		String targetFileStr = targetFolder.getPath()+"\\" + fileNameStr + ".txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+			String name; 
+			System.out.print("Quando enviar uma mensagem vazia, o programa é encerrado: ");
+			do {
+				
+				name = input.nextLine();
+				bw.write(name);
+				bw.newLine();
+				if (name.isEmpty())
+					break;
+				} 
+			while (true);
+			
+		}catch (IOException e) {
+			System.out.println("Péssimas notícias, meu caro!");
+			}
+		
+		System.out.println("Acabou o programa!");
+
+		input.close();
+
+	}
+}
